9f760281e6daacc4d3ca85cee5f6c906098f8363
Dani <danidelrosario7@gmail.com>
2024-11-08 11:10:31 +0000

    test

--- src/Bomb.java
+++ src/Bomb.java
@@ -2,6 +2,7 @@ import bagel.Image;
 import bagel.util.Point;
 
 public class Bomb extends GameEntity {
+    //test
     private final static Image BOMB_IMAGE = new Image("res/bomb.png");
     public Bomb(Point topLeft) {
         super(topLeft);
